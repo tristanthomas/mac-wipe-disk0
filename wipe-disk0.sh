7d1a078d50b607c74c17aab0375c96b6e2084888
Tristan Thomas <tristanthomas@users.noreply.github.com>
2017-10-06 13:39:32 -0700

    Update for macOS High Sierra compatibility

--- wipe-disk0.sh
+++ wipe-disk0.sh
@@ -7,45 +7,28 @@
 # Execute this script under macOS Recovery from a bootable USB drive to wipe and repartition disk0.
 
 # Checks if the computer is booted under macOS Recovery
-BOOT_MODE=$(system_profiler SPSoftwareDataType 2>/dev/null | grep 'Boot Mode: Booted from installation CD/DVD')
-if [[  "${BOOT_MODE}" == "" ]] ; then
+BOOT_MODE=$(sysctl -a | grep kern.bootsignature | awk '{print $2}')
+if [[  "${BOOT_MODE}" != "" ]] ; then
 	echo "WARNING: This script wipes disk0 if executed. If you intended to wipe disk0, execute this script under macOS Recovery from a bootable macOS USB install drive."
 	exit 130
 fi
-
-# Locate the recovery partition
-RECOVERY_PARTITION_ID=$(diskutil list | grep Apple_Boot | grep disk0 | awk '{print $7}')
 
 # Force unmount disk0 and disk1
 diskutil quiet unmountDisk force /dev/disk1
 diskutil quiet unmountDisk force /dev/disk0
 
-# Check the status of FileVault
-FILEVAULT_ON=$(diskutil cs list | grep disk0 -A 8 | grep 'Conversion Status' | awk -F ' ' '{print $3}')
-
-# If the drive is encrypted with FileVault, just the recovery partition will be wiped.
-# If FileValult is not enabled, the entire drive will be wiped.
-
-if [[  "${FILEVAULT_ON}" == "Complete" ]] ; then
-	echo "Wiping the recovery partition /dev/"${RECOVERY_PARTITION_ID}
-	diskutil zeroDisk /dev/${RECOVERY_PARTITION_ID} || {
-		echo "Failed to wipe the recovery partition /dev/"${RECOVERY_PARTITION_ID}
-		exit 131
-	}
-	echo "The recovery partition has been wiped."
-else
-	echo "FileVault is not enabled, wiping the entire drive."
+echo "Wiping the internal drive, disk0..."
 	diskutil zeroDisk /dev/disk0 || {
 		echo "Failed to wipe drive /dev/disk0"
 		exit 132
 	}
 	echo "The entire drive has been wiped."
-fi
 
-diskutil partitionDisk /dev/disk0 GPT JHFS+ "Macintosh HD" 0b > /dev/null || {
+diskutil partitionDisk /dev/disk0 GPT APFS "Macintosh HD" 0b > /dev/null || {
 	echo "Failed to format drive /dev/disk0"
 	exit 133
 }
 
 # Output the system serial number
-system_profiler SPHardwareDataType | grep "Serial Number (system)" | sed -e 's/^ *//'
+SERIAL_NUMBER=$(ioreg -l | grep IOPlatformSerialNumber | awk -F \" '{print $4}')
+echo "Serial Number: $SERIAL_NUMBER"
